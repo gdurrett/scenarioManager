7a85eb3b1732d6a3f006b38e223d52d84762eb7c
Greg Durrett <gdurrett@gregs-imac.local>
2017-09-11 09:34:50 -0700

    Add missing files

---
+++ Scenario Manager/AddCampaignTitleCell.swift
@@ -0,0 +1,37 @@
+//
+//  AddCampaignTitleCell.swift
+//  Scenario Manager
+//
+//  Created by Greg Durrett on 9/8/17.
+//  Copyright © 2017 AppHazard Productions. All rights reserved.
+//
+
+import UIKit
+
+class AddCampaignTitleCell: UITableViewCell {
+    
+    @IBOutlet weak var campaignTitleTextField: UITextField!
+    
+    func configure(withViewModel viewModel: AddCampaignTitleCellViewModel) {
+        campaignTitleTextField.placeholder = viewModel.campaignTitleTextFieldPlaceholder
+    }
+    
+    static var nib:UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
